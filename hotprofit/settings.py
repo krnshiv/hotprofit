b6d23a57edad0cb492cd8280b8dc8ccfb2f8ee4d
krnshiv <krnshiv@gmail.com>
2020-03-06 20:00:16 +0530

    update heroku 1.01

--- hotprofit/settings.py
+++ hotprofit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = '+7*7+8y+z7t5g8(v$wmbh_yv6d!+_alk9dtdm&q5ru4i0-mq4!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
